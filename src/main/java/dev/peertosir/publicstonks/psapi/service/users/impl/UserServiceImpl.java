0e598b78ab1532377fd493c9bddb50961ea9b070
Ilya Danilov <peertosir@gmail.com>
2020-05-15 01:46:30 +0300

    Added auth login/sign up. added filter for routes

--- src/main/java/dev/peertosir/publicstonks/psapi/service/users/impl/UserServiceImpl.java
+++ src/main/java/dev/peertosir/publicstonks/psapi/service/users/impl/UserServiceImpl.java
@@ -4,26 +4,68 @@ import dev.peertosir.publicstonks.psapi.entity.UserEntity;
 import dev.peertosir.publicstonks.psapi.repository.UserRepository;
 import dev.peertosir.publicstonks.psapi.service.users.UserService;
 import dev.peertosir.publicstonks.psapi.shared.dto.UserDto;
+import dev.peertosir.publicstonks.psapi.shared.utils.HashIdGenerator;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class UserServiceImpl implements UserService {
+
     private final UserRepository userRepository;
+    private final HashIdGenerator hashIdGenerator;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, HashIdGenerator hashIdGenerator,
+                           BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
+        this.hashIdGenerator = hashIdGenerator;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
     public UserDto createUser(UserDto userDto) {
+        //Check if user with this email already exists
+        if (userRepository.findByEmail(userDto.getEmail()) != null) {
+            throw new RuntimeException("Record already exists");
+        }
+
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(userDto, userEntity);
+
+        String publicUserId = hashIdGenerator.generateUserId(20);
+        userEntity.setUserId(publicUserId);
+        userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(userDto.getPassword()));
         UserEntity savedUser = userRepository.save(userEntity);
+
         UserDto returnValue = new UserDto();
         BeanUtils.copyProperties(savedUser, returnValue);
         return returnValue;
+    }
+
+    @Override
+    public UserDto getUser(String email) {
+        UserEntity userEntity = userRepository.findByEmail(email);
+        if (userEntity == null) throw new UsernameNotFoundException(email);
+
+        UserDto returnValue = new UserDto();
+        BeanUtils.copyProperties(userEntity, returnValue);
+        return returnValue;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByEmail(email);
+
+        if (userEntity == null) throw new UsernameNotFoundException(email);
+
+        return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), new ArrayList<>());
     }
 }
---
+++ src/main/java/dev/peertosir/publicstonks/psapi/shared/utils/HashIdGenerator.java
@@ -0,0 +1,25 @@
+package dev.peertosir.publicstonks.psapi.shared.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+@Component
+public class HashIdGenerator {
+    private final Random RANDOM = new SecureRandom();
+    private final String ALPHABET = "01234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public String generateUserId(int length) {
+        return generateRandomString(length);
+    }
+
+    private String generateRandomString(int length) {
+        StringBuilder returnValue = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            returnValue.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
+        }
+        return new String(returnValue);
+    }
+}
